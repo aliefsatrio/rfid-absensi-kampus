f625e3b255142f44f5d7c35da0a9c939bb362919
Yusuf Maulana <yusufmaulanasyifa25@gmail.com>
2026-07-01 21:43:43 +0700

    Merge origin/dev-mustofa-achmad into dev-yusuf-maulana-sifa
    # Conflicts:
    #	src/main/java/com/rfid/absensikampus/GenericDAO.java

---
+++ src/main/java/services/Settings.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package services;
+
+/**
+ *
+ * @author yusuf
+ */
+class Settings {
+
+    static class prefs {
+
+        static String get(String language, String id) {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
+        public prefs() {
+        }
+    }
+    
+}
